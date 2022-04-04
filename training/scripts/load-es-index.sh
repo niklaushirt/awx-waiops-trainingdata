244500a1776e6e96957547bde2bb7a01f16a1dbc
Niklaus Hirt <niklaushirt@gmail.com>
2022-04-04 09:22:35 +0200

    3.3

--- training/scripts/load-es-index.sh
+++ training/scripts/load-es-index.sh
@@ -40,8 +40,9 @@ echo "     ---------------------------------------------------------------------
 oc project $WAIOPS_NAMESPACE > /dev/null 2>&1	
 
 
-export username=$(oc get secret $(oc get secrets | grep ibm-aiops-elastic-secret | awk '!/-min/' | awk '{print $1;}') -o jsonpath="{.data.username}"| base64 --decode)	
-export password=$(oc get secret $(oc get secrets | grep ibm-aiops-elastic-secret | awk '!/-min/' | awk '{print $1;}') -o jsonpath="{.data.password}"| base64 --decode)	
+export username=$(oc get secret $(oc get secrets | grep aiops-elastic-secret | awk '!/-min/' | awk '{print $1;}') -o jsonpath="{.data.username}"| base64 --decode)	
+export password=$(oc get secret $(oc get secrets | grep aiops-elastic-secret | awk '!/-min/' | awk '{print $1;}') -o jsonpath="{.data.password}"| base64 --decode)	
+
 
 export WORKING_DIR_ES="./$APP_NAME/$VERSION/$INDEX_TYPE"	
 
